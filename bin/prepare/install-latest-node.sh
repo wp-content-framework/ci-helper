858078f5d1e58d3173ac414c16db9a791f41070f
Technote <technote.space@gmail.com>
2019-07-11 15:55:56 +0900

    update

--- bin/prepare/install-latest-node.sh
+++ bin/prepare/install-latest-node.sh
@@ -5,6 +5,14 @@ set -e
 #NODE_JS_VERSION=${NODE_JS_VERSION:-node}
 NODE_JS_VERSION=${NODE_JS_VERSION:-11}
 
-curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh
-source ~/.nvm/nvm.sh && nvm install ${NODE_JS_VERSION} && nvm alias default ${NODE_JS_VERSION}
-node --version
+export NVM_DIR="${HOME}/.nvm"
+curl -o ${NVM_DIR}/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh
+
+[[ -s "${NVM_DIR}/nvm.sh" ]] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+[[ -s "${NVM_DIR}/bash_completion" ]] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+
+nvm install ${NODE_JS_VERSION}
+nvm alias default ${NODE_JS_VERSION}
+nvm use default
+
+node -v
