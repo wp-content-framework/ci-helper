f7c7a959d0f9174c76885fe10a0e045c53e5ac9e
Technote <technote.space@gmail.com>
2019-07-11 13:38:04 +0900

    update script

--- bin/prepare/install-latest-node.sh
+++ bin/prepare/install-latest-node.sh
@@ -5,11 +5,6 @@ set -e
 #NODE_JS_VERSION=${NODE_JS_VERSION:-node}
 NODE_JS_VERSION=${NODE_JS_VERSION:-11}
 
-if [[ ! -d ~/.nvm/.git ]]; then
-    rm -rf ~/.nvm
-    git clone https://github.com/creationix/nvm.git ~/.nvm
-fi
-git -C ~/.nvm checkout $(git -C ~/.nvm describe --abbrev=0 --tags)
-
+curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh
 source ~/.nvm/nvm.sh && nvm install ${NODE_JS_VERSION} && nvm alias default ${NODE_JS_VERSION}
 node --version
