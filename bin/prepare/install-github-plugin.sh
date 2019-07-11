0ce758f3540cfaf0c661c7971339bd477c942080
Technote <technote.space@gmail.com>
2019-07-11 16:26:28 +0900

    update

--- bin/prepare/install-github-plugin.sh
+++ bin/prepare/install-github-plugin.sh
@@ -25,7 +25,6 @@ if [[ -f ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}/composer.json ]]; then
     composer install -n --working-dir=${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG} --no-dev
 fi
 
-node --version
 if [[ -f ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}/package.json ]]; then
     yarn --cwd ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG} --audit install
     yarn --cwd ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG} build
