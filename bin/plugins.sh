d873f1763eeed7b142f9f8041242bcd4e5775827
Technote <technote.space@gmail.com>
2019-07-11 16:07:26 +0900

    revert

--- bin/plugins.sh
+++ bin/plugins.sh
@@ -5,10 +5,10 @@
 set -e
 
 org_plugins=()
-#org_plugins+=( "wordpress-seo" )
+org_plugins+=( "wordpress-seo" )
 org_plugins+=( "akismet" )
 org_plugins+=( "contact-form-7" )
-#org_plugins+=( "jetpack" )
+org_plugins+=( "jetpack" )
 org_plugins+=( "woocommerce" )
 org_plugins+=( "wordpress-importer" )
 org_plugins+=( "duplicate-post" )
