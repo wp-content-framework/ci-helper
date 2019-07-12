43379ebf185635f3659a418bb9845740aa87e0b4
Technote <technote.space@gmail.com>
2019-07-13 02:40:57 +0900

    chore: add release body

--- bin/deploy/env.sh
+++ bin/deploy/env.sh
@@ -5,4 +5,5 @@ set -e
 export RELEASE_FILE=${REPO_NAME}.zip
 export RELEASE_TITLE=${TRAVIS_TAG}
 export RELEASE_TAG=${TRAVIS_TAG}
+export RELEASE_BODY="Auto updated (Travis build: ${TRAVIS_BUILD_WEB_URL})"
 export GH_PAGES_DIR=${TRAVIS_BUILD_DIR}/gh-pages
