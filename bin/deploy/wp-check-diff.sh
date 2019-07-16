c638642727d40fa3a1c1ec2678a17b3aed745009
Technote <technote.space@gmail.com>
2019-07-16 13:39:47 +0900

    revert

--- bin/deploy/wp-check-diff.sh
+++ bin/deploy/wp-check-diff.sh
@@ -6,21 +6,18 @@ current=$(cd $(dirname $0);
 pwd)
 source ${current}/../variables.sh
 
-echo ""
-echo ">> Nothing to do..."
+bash ${SCRIPT_DIR}/deploy/prepare_svn.sh
 
-#bash ${SCRIPT_DIR}/deploy/prepare_svn.sh
-#
-#if [[ ! -d ${SVN_DIR} ]]; then
-#	exit;
-#fi
-#
-#pushd ${SVN_DIR}
-#
-#echo ""
-#echo ">> Run svn st."
-#svn st
-#
-#pushd
-#
-#bash ${current}/clear_work_dir.sh
+if [[ ! -d ${SVN_DIR} ]]; then
+	exit;
+fi
+
+pushd ${SVN_DIR}
+
+echo ""
+echo ">> Run svn st."
+svn st
+
+pushd
+
+bash ${current}/clear_work_dir.sh
