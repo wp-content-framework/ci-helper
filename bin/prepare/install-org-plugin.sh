2adeb7e8da80f25abf7152103a6de67f8b9a1d89
Technote <technote.space@gmail.com>
2019-09-07 00:54:13 +0900

    feat: consider required version (#21)

--- bin/prepare/install-org-plugin.sh
+++ bin/prepare/install-org-plugin.sh
@@ -5,8 +5,8 @@
 set -e
 
 if [[ $# -lt 1 ]]; then
-	echo "usage: $0 <plugin slug> [plugin version]"
-	exit 1
+  echo "usage: $0 <plugin slug> [plugin version]"
+  exit 1
 fi
 
 set -x
@@ -14,19 +14,32 @@ set -x
 PLUGIN_SLUG=$1
 PLUGIN_VERSION=${2-latest}
 
-if [[ ${PLUGIN_VERSION} = "latest" ]]; then
-    WP_PLUGIN=$(curl "https://api.wordpress.org/plugins/info/1.0/${PLUGIN_SLUG}.json" | jq -r .download_link)
+if [[ ${PLUGIN_VERSION} == "latest" ]]; then
+  WP_PLUGIN=$(curl "https://api.wordpress.org/plugins/info/1.0/${PLUGIN_SLUG}.json" | jq -r .download_link)
 else
-    WP_PLUGIN=https://downloads.wordpress.org/plugin/${PLUGIN_SLUG}.${PLUGIN_VERSION}.zip
+  WP_PLUGIN=https://downloads.wordpress.org/plugin/${PLUGIN_SLUG}.${PLUGIN_VERSION}.zip
 fi
 
 FILE_NAME=${WP_PLUGIN##*/}
+README=${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}/readme.txt
 
 if [[ ! -f ${TRAVIS_BUILD_DIR}/.plugin/${FILE_NAME} ]]; then
-    rm -f ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}.*.zip
-    rm -rdf ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}
-    curl -s ${WP_PLUGIN} -o ${TRAVIS_BUILD_DIR}/.plugin/${FILE_NAME}
+  rm -f ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}.*.zip
+  rm -rdf ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}
+  curl -s ${WP_PLUGIN} -o ${TRAVIS_BUILD_DIR}/.plugin/${FILE_NAME}
 fi
 if [[ ! -d ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG} ]]; then
-    unzip ${TRAVIS_BUILD_DIR}/.plugin/${FILE_NAME} -d ${TRAVIS_BUILD_DIR}/.plugin
+  unzip ${TRAVIS_BUILD_DIR}/.plugin/${FILE_NAME} -d ${TRAVIS_BUILD_DIR}/.plugin
+fi
+
+if [[ -n "${WP_VERSION}" && ${WP_VERSION} != "latest" ]]; then
+  REQUIRED_VERSION=$(cat ${README} | grep "Requires at least" | sed -e 's/Requires at least:\s*//')
+  if [[ -n "${REQUIRED_VERSION}" ]]; then
+    echo "Required version: ${REQUIRED_VERSION}"
+    echo "WP version: ${WP_VERSION}"
+    if [[ "${REQUIRED_VERSION}" != $(echo -e "${REQUIRED_VERSION}\n${WP_VERSION}" | sort -V | head -n1) ]]; then
+      echo "Not enough version..."
+      rm -rdf ${TRAVIS_BUILD_DIR}/.plugin/${PLUGIN_SLUG}
+    fi
+  fi
 fi
