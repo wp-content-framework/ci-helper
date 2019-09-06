83b534e3d04c5d0f996d245259d6c1fe1ccbb5d1
Technote <technote.space@gmail.com>
2019-09-07 00:53:15 +0900

    chore: composer

--- bin/php/install-composer.sh
+++ bin/php/install-composer.sh
@@ -9,7 +9,7 @@ source ${current}/../variables.sh
 echo ""
 echo ">> Run composer install."
 if [[ ! -z "${IGNORE_PLATFORM_REQS}" ]]; then
-	composer install -n --working-dir=${TRAVIS_BUILD_DIR} --ignore-platform-reqs
+	composer install -n --working-dir=${TRAVIS_BUILD_DIR} --no-interaction --prefer-dist --no-suggest --ignore-platform-reqs
 else
-	composer install -n --working-dir=${TRAVIS_BUILD_DIR}
+	composer install -n --working-dir=${TRAVIS_BUILD_DIR} --no-interaction --prefer-dist --no-suggest
 fi
