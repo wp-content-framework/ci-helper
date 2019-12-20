2605e54ae5d2d2e70979ac8dea2947097d979e92
Technote <technote.space@gmail.com>
2019-12-20 22:38:07 +0900

    Update package.sh

--- bin/update/package.sh
+++ bin/update/package.sh
@@ -27,10 +27,9 @@ fi
 if [[ -z $(command -v ncu) ]]; then
   npm install -g npm-check-updates
 fi
-ncu -u --packageFile ${working_dir}/package.json
-if [[ -n "${GITHUB_ACTION}" ]]; then
 
-else
+ncu -u --packageFile ${working_dir}/package.json
+
 if [[ -n "${GITHUB_ACTION}" ]]; then
   sudo chown -R $USER:$GROUP $HOME/.npm
   sudo chown -R $USER:$GROUP $HOME/.config
@@ -39,5 +38,6 @@ else
     yarn --cwd ${working_dir} cache clean
   fi
 fi
+
 yarn --cwd ${working_dir} install
 yarn --cwd ${working_dir} upgrade
