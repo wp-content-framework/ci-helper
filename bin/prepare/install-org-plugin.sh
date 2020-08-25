e95dc86a8c1563168fbc5650c65f915f963b06a9
Technote <technote.space@gmail.com>
2020-08-26 02:17:19 +0900

    chore: consider mac

--- bin/prepare/install-org-plugin.sh
+++ bin/prepare/install-org-plugin.sh
@@ -38,10 +38,10 @@ if [[ ! -f ${README} ]]; then
 fi
 
 if [[ -n "${WP_VERSION}" && ${WP_VERSION} =~ ^[0-9]+\.[0-9]+$ && -z "${IGNORE_PLUGIN_VERSION}" ]]; then
-  UPPER_CASE_SLUG=${PLUGIN_SLUG^^}
+  UPPER_CASE_SLUG=$(echo ${PLUGIN_SLUG} | tr "[a-z]" "[A-Z]")
   IGNORE_EACH_PLUGIN_VERSION=$(eval echo '$'IGNORE_${UPPER_CASE_SLUG//-/_}_VERSION)
   if [[ -z "${IGNORE_EACH_PLUGIN_VERSION}" ]]; then
-    REQUIRED_VERSION=$(cat ${README} | grep "Requires at least" | sed -e 's/Requires at least:\s*//')
+    REQUIRED_VERSION=$(cat ${README} | grep "Requires at least" | sed -e 's/Requires at least: *//')
     if [[ -n "${REQUIRED_VERSION}" ]]; then
       echo "Required version: ${REQUIRED_VERSION}"
       echo "WP version: ${WP_VERSION}"
